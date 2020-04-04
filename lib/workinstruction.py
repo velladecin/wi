64da175481fbd6ccf0ab3cf2fdea084b58cf54b7
velladecin <velladecin@hotmail.com>
2020-04-04 21:07:23 +1100

    added editing of topics, moved wi related stuff from Shell to wi

--- lib/workinstruction.py
+++ lib/workinstruction.py
@@ -2,6 +2,7 @@
 import os
 import re
 import sys
+import subprocess
 from glob import glob
 from pprint import pprint
 
@@ -27,12 +28,40 @@ class Workinstruction:
     def getTopics(self):
         return self.topics
 
-    def getTopic(self, t):
+    def isTopic(self, t):
+        try:
+            self.topics[t]
+            return True
+        except KeyError:
+            return False
+
+    def readTopic(self, t):
+        topic = self.__getTopic(t)
+
+        try:
+            with open(topic, 'r') as f:
+                data = f.readlines()
+        except (FileNotFoundError, TypeError):
+            # Somebody removed our file, or topic does not exist.
+            # This *should* not happen but..
+            print("Could not retrieve topic: %s" % t)
+            data = []
+
+        return data
+
+    def printTopic(self, t):
+        for line in self.readTopic(t):
+            print(line.rstrip())
+
+    def editTopic(self, t):
+        topic = self.__getTopic(t)
+        subprocess.run(["vi", topic])
+
+    def __getTopic(self, t):
         try:
             return self.topics[t]
         except KeyError:
             return None
-
 
 ##
 ## Shell
@@ -48,53 +77,24 @@ class WiShell:
         self.prompt = prompt
         self.history = history
 
-    def __help(self, cmd=None):
-        cmds = {
-            "ls": "ls                    - list work instructions (wi)",
-            "lt": "lt <wi>               - list topics for <wi>",
-            "vt": "vt <wi> <topic>       - view <topic> for <wi>",
-            "et": "et <wi> <topic>       - edit <topic> for <wi>"
-        }
-
-        try:
-            print(cmds[cmd])
-        except KeyError:
-            for c in cmds:
-                print(cmds[c])
-
-    def __cmdOK(self, cmd, args, require_arg=0):
-        if require_arg > 0:
-            if not args:
-                self.__help(cmd)
-                return False
-
-            try:
-                args[require_arg-1]
-            except IndexError:
-                self.__help(cmd)
-                return False
-
-        return True
-
     def run(self):
         currentTopic = None # topic cache
         cmdHistory = []
 
-        keep_running = True
-        while keep_running:
+        while True:
             uinput = input("%s" % self.prompt).split()
             try:
                 cmd = uinput[0]
                 args = uinput[1:]
                 cmdHistory.append(uinput)
             except IndexError:
-                cmd = "help"
+                continue
 
             if len(cmdHistory) > self.history:
                 cmdHistory = cmdHistory[1:] # remove oldest (first) element 
 
-            if cmd == "exit" or cmd == "quit":
-                keep_running = False
+            if cmd == "exit" or cmd == "quit" or cmd == "q" or cmd == "x":
+                break
 
             if cmd == "help":
                 self.__help()
@@ -131,7 +131,7 @@ class WiShell:
 
                 continue
 
-            if cmd == "vt":
+            if cmd == "vt" or cmd == "et":
                 if currentTopic and len(args) < 2:
                     args.insert(0, currentTopic)
 
@@ -145,24 +145,65 @@ class WiShell:
                         print("%s not found" % w)
                         continue
 
-                    topic = wi.getTopic(t)
-                    if not topic:
-                        print("%s not found" % t)
+                    if not wi.isTopic(t):
+                        print("%s:%s not found" % (w, t))
                         continue
 
-                    try:
-                        with open(topic, 'r') as f:
-                            data = f.readlines()
-                    except FileNotFoundError:
-                        # somebody removed our file..
-                        print("Could not retrieve topic: %s" % t)
-                        continue
+                    if cmd == "vt":
+                        wi.printTopic(t)
 
-                    for line in data:
-                        print(line.rstrip())
+                    if cmd == "et":
+                        wi.editTopic(t)
 
                 continue
 
-            if cmd == "et":
-                print("edit topic")
+    #
+    # Private ;)
+
+    def __help(self, cmd=None):
+        cmds = {
+            "ls": """
+ls
+    list all known work instructions (wi)""",
+            "lt": """
+lt <wi> [<wi> ..]
+    List all known topics for <wi>.
+    When single <wi> used, it is cached and implied for subsequent commands (requiring <wi> as argument) and <wi> can then be omitted in those commands.
+    When multiple <wi> used, no caching and/or implication take place.""",
+            "vt": """
+vt <wi> <topic>
+    view <topic> for <wi>""",
+            "et": """
+et <wi> <topic>
+    edit <topic> for <wi>""",
+            "ct": """
+ct <wi> <topic>
+    create <topic> for <wi>""",
+            "exit": """
+exit|x
+    exit program""",
+            "quit": """
+quit|q
+    same as exit"""
+        }
+
+        try:
+            print(cmds[cmd])
+        except KeyError:
+            for c in cmds:
+                print(cmds[c])
+
+    def __cmdOK(self, cmd, args, require_arg=0):
+        if require_arg > 0:
+            if not args:
+                self.__help(cmd)
+                return False
+
+            try:
+                args[require_arg-1]
+            except IndexError:
+                self.__help(cmd)
+                return False
+
+        return True
 
